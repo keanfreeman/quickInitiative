5ad2cd6393a6feb1b0d4725c530c75930319270c
Kean Freeman <keanfree@umich.edu>
2017-08-22 23:41:10 -0700

    added functionality - the 'name' fields now auto-select the entire name when the field is clicked

--- js/directives/creatureInfo.js
+++
@@ -1,9 +0,0 @@
-app.directive('creatureInfo', function() { 
-  return { 
-    restrict: 'E', 
-    scope: { 
-      details: '=' 
-    }, 
-    templateUrl: 'js/directives/creatureInfo.html' 
-  }; 
-});
